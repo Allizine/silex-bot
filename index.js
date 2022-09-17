8c74691f66fd6ab79ab618cba6fbffd7fcc286da
Julexar <viktor.thorin@gmail.com>
2022-09-17 12:45:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-// require discordjs classes
-const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
-const { token, Database } = require('./config.json');
-const client = new Client({intents: 8});
+const DiscordClient = require('./structures/lib/DiscordClient');
+const client = new DiscordClient();
 
+client.start();
 
-client.commands = new Collection()
-require("./Handlers/Events")(client);
-require("./Handlers/Commands")(client);
-
-
-
-// Login to Discord with your bot token
- client.login(token);
-
-
-
+module.exports = client;
