c5efaf87121e43eb2005b70286a3b1cee67e0266
Julexar <viktor.thorin@gmail.com>
2022-09-17 12:45:41 +0200

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    token: 'OTk4ODkwMTkzNjM5NzE0ODU4.GxP9Ck.9X_Ju5FSlxGoX-c37x7G1Uk6LjXzJEZD7TXH9s',
+    default_prefix: '!',
+    owners: ['676518256282042393'],
+    presence: {
+        activities: { 
+            name: 'watching over Silex Community',
+            type: 'WATCHING',
+        },
+        status: 'online',
+    },
+};
