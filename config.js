9e50967b7633970011ba6c554d98c9ec168d0ae2
Julexar <viktor.thorin@gmail.com>
2022-09-24 13:25:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,12 @@ module.exports = {
     default_prefix: '!',
     owners: ['676518256282042393'],
     presence: {
-        activities: { 
-            name: 'watching over Silex Community',
-            type: 'WATCHING',
-        },
+        activities: [
+          {
+            name: 'Silex Community',
+            type: 3,
+          },
+        ],
         status: 'online',
     },
 };
