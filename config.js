94055da402782e4c09628561a4243b50995a876e
Julexar <viktor.thorin@gmail.com>
2022-09-30 11:08:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'OTk4ODkwMTkzNjM5NzE0ODU4.Gt7S0J.5EX-gQ4B5KcTrSpQ2ZYFv5te4qeVD46MGzhYCg',
+    token: 'TOKEN HERE',
     default_prefix: 's!',
     owners: ['676518256282042393','765859014424395777'],
     presence: {
