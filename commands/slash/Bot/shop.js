968571072fa75e718cda04ab6257d146e18d94ac
Julexar <viktor.thorin@gmail.com>
2022-09-30 09:47:15 +0200

    Update shop.js

--- commands/slash/Bot/shop.js
+++ commands/slash/Bot/shop.js
@@ -18,12 +18,6 @@ module.exports = {
           type: ApplicationCommandOptionType.String,
           required: true,
         },
-        {
-          name: "amount",
-          description: "Provide a Number.",
-          type: ApplicationCommandOptionType.Integer,
-          required: false,
-        },
       ],
     },
     {
@@ -32,7 +26,7 @@ module.exports = {
       type: ApplicationCommandOptionType.Subcommand,
       options: [
         {
-          name: "item name",
+          name: "name",
           description: "Provide the name of an Item.",
           type: ApplicationCommandOptionType.String,
           required: false,
@@ -48,11 +42,60 @@ module.exports = {
 
         break;
       case 'list':
-        var inventory = [];
-        var inv = fs.readFileSync("./database/shop.json").data;
-        for (let data in inv) {
-          console.log(data);
+        var shop = [];
+        var info = JSON.parse(fs.readFileSync("./database/shop.json"));
+        shop.push(info);
+        const row = new ActionRowBuilder()
+          .addComponents(
+            new ButtonBuilder()
+              .setCustomId('shopprev')
+              .setStyle(ButtonStyle.Primary)
+              .setEmoji('⏪')
+              .setDisabled(true),
+            new ButtonBuilder()
+              .setCustomId('shopnext')
+              .setStyle(ButtonStyle.Primary)
+              .setEmoji('⏩')
+          );
+        const list1 = new EmbedBuilder()
+          .setColor('#00ffff')
+          .setAuthor({ name: "Page 1/2" })
+          .setTitle(`${shop[0].name}`)
+        const list2 = new EmbedBuilder()
+          .setColor('#00ffff')
+          .setAuthor({ name: "Page 2/2" })
+          .setTitle(`${shop[0].name}`)
+        for (let i = 0; i < shop[0].inventory.length; i++) {
+          let emoji = shop[0].inventory[i].emoji;
+          let name = shop[0].inventory[i].name;
+          let price = shop[0].inventory[i].price;
+          if (i < 4) {
+            list1.addFields(
+              { name: "Name", value: `${emoji} ${name}`, inline: true },
+              { name: "Price", value: `${price}`, inline: true },
+              { name: "\u200B", value: "\u200B" }
+            );
+          } else if (i >= 4) {
+            list2.addFields(
+              { name: "Name", value: `${emoji} ${name}`, inline: true },
+              { name: "Price", value: `${price}`, inline: true },
+              { name: "\u200B", value: "\u200B" }
+            );
+          }
         }
+        interaction.reply({ embeds: [list1], components: [row] });
+        const collector = interaction.channel.createMessageComponentCollector({ time: 300000 });
+        collector.on("collect", async (i) => {
+          if (i.customId == "next") {
+            row.components[1].setDisabled(true);
+            row.components[0].setDisabled(false);
+            await i.editReply({ embeds: [list2], components: [row] });
+          } else if (i.customId == "prev") {
+            row.components[1].setDisabled(false);
+            row.components[0].setDisabled(true);
+            await i.editReply({ embeds: [list1], components: [row] });
+          }
+        });
         break;
     }
   },
